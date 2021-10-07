271c541f3700c7a8c8b3b7a7ee77e503df9fa8c0
Ikeaba Ngozi <kebidegozi@gmail.com>
2021-10-07 11:09:35 +0100

    push

---
+++ app/Http/Controllers/Backend/AdminProfileController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Admin;
+use Auth;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Hash;
+
+class AdminProfileController extends Controller
+{
+    public function AdminProfile()
+    {
+        $admin_data = Admin::find(1);
+
+        return view('admin.pages.profile_view', compact('admin_data'));
+    }
+
+    public function AdminProfileEdit()
+    {
+        $edit_data = Admin::find(1);
+
+        return view('admin.pages.profile_edit', compact('edit_data'));
+    }
+
+    public function AdminProfileStore(Request $request)
+    {
+
+        $validated = $request->validate(
+            [
+                'name' => 'required',
+                'email' => 'required',
+                'profile_photo_path' => 'required|mimes:jpg,jpeg,png',
+            ],
+
+        );
+
+        $admin = Admin::find(1);
+        if ($admin) {
+
+            $admin->name = $request->name;
+            $admin->email = $request->email;
+
+            if ($request->file('profile_photo_path')) {
+                $file = $request->file('profile_photo_path');
+                @unlink(public_path('upload/admin_images/' . $admin->profile_photo_path));
+                $filename = date('YmdHi') . $file->getClientOriginalName();
+                $file->move(public_path('upload/admin_images'), $filename);
+                $admin['profile_photo_path'] = $filename;
+            }
+
+            $admin->save();
+
+            return redirect()->route('admin.profile')->with('success', 'Profile has been Updated Successfully');
+        } else {
+
+            $admin->name = $request->name;
+            $admin->email = $request->email;
+
+            $admin->save();
+
+            return redirect()->route('admin.profile')->with('success', 'Profile has been Updated Successfully');
+        }
+    }
+
+    public function AdminPassword()
+    {
+        $admin_password = Admin::find(1);
+
+        return view('admin.pages.change_password', compact('admin_password'));
+    }
+
+    public function UpdatePassword(Request $request)
+    {
+
+        $validated = $request->validate(
+            [
+                'current_password' => 'required',
+                'password' => 'required|regex:/^.*(?=.{6,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\x])(?=.*[!$#@_&*%]).*$/|confirmed',
+            ],
+
+        );
+
+        $hashedPassword = Admin::find(1)->password;
+
+        if (Hash::check($request->current_password, $hashedPassword)) {
+
+            $admin = Admin::find(1);
+
+            $admin->password = Hash::make($request->password);
+
+            $admin->save();
+
+            Auth::logout();
+
+            return redirect()->route('admin.logout')->with('success', 'Password is Changed Successfully');
+        } else {
+
+            return redirect()->back()->with('error', 'Current Password is Invalid');
+        }
+    }
+}
---
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Redirect;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        return view('frontend.index');
+    }
+
+    public function UserLogout()
+    {
+        Auth::logout();
+
+        return redirect()->route('login');
+    }
+
+    public function UserProfile()
+    {
+        $id = Auth::user()->id;
+        $user = User::find($id);
+
+        return view('frontend.pages.profile', compact('user'));
+        return view('frontend.body.profilesiderbar', compact('user'));
+    }
+
+    public function UpdateProfile(Request $request)
+    {
+        $validated = $request->validate(
+            [
+                'name' => 'required',
+                'email' => 'required',
+                'phone' => 'required',
+            ],
+
+        );
+
+        $id = Auth::user()->id;
+        $user = User::find($id);
+
+        if ($user) {
+
+            $user->name = $request['name'];
+            $user->phone = $request['phone'];
+            $user->email = $request['email'];
+
+            $user->save();
+
+            return redirect()->back();
+        }
+    }
+
+    public function UpdatePicture(Request $request)
+    {
+
+        $validated = $request->validate(
+            [
+                'profile_photo_path' => 'required|mimes:jpg,jpeg,png',
+            ],
+
+        );
+
+        $id = Auth::user()->id;
+        $user = User::find($id);
+
+        if ($user) {
+
+            if ($request->file('profile_photo_path')) {
+                $file = $request->file('profile_photo_path');
+                if (file_exists('upload/user_images/' . $user->profile_photo_path)) {
+                    @unlink(public_path('upload/user_images/' . $user->profile_photo_path));
+                    $filename = date('YmdHi') . $file->getClientOriginalName();
+                    $file->move(public_path('upload/user_images'), $filename);
+                    $user['profile_photo_path'] = $filename;
+                } else {
+                    $filename = date('YmdHi') . $file->getClientOriginalName();
+                    $file->move(public_path('upload/user_images'), $filename);
+                    $user['profile_photo_path'] = $filename;
+                }
+            }
+
+            $user->save();
+
+            return redirect()->back();
+        }
+    }
+
+    public function UpdatePassword(Request $request)
+    {
+        $validated = $request->validate(
+            [
+                'current_password' => 'required',
+                'password' => 'required|regex:/^.*(?=.{6,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\x])(?=.*[!$#@_&*%]).*$/|confirmed',
+            ],
+
+        );
+
+        $id = Auth::user()->id;
+
+        $hashedPassword = User::find($id)->password;
+
+        if (Hash::check($request->current_password, $hashedPassword)) {
+
+            $user = User::find($id);
+
+            $user->password = Hash::make($request->password);
+
+            $user->save();
+
+            Auth::logout();
+
+            return redirect()->route('user.logout');
+        } else {
+            return redirect()->route('dashboard');
+        }
+    }
+}
